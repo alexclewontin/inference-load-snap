4bd055c6d3a30c69bb4427fb18c0b715d0102633
Alex Lewontin <alex.lewontin@canonical.com>
2024-05-02 12:09:21 -0400

    Better interaction with microk8s snap
    Signed-off-by: Alex Lewontin <alex.lewontin@canonical.com>

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if ! snapctl is-connected k8s-credentials; then
+    >&2 echo "ERROR: Please make sure microk8s is installed, run \`snap connect $SNAP_INSTANCE_NAME:k8s-credentials microk8s:microk8s\`, and try again"
+    exit 1
+fi
+
 export KUBECONFIG=$SNAP/microk8s/credentials/client.config
 
 kubectl apply -f $SNAP/deploy/istio-namespace.yaml
